e1db44ac178ab18a392ea57786b63a22074b88a4
Loser Cheems <losercheems@gmail.com>
2026-02-25 17:52:06 +0800

    Refactor __init__.py to include Triton function imports and define __all__ for public API

---
+++ omni_moe/ops/triton/__init__.py
@@ -0,0 +1,11 @@
+from .interface import (
+    triton_omni_mlp_func,
+    triton_omni_router_func,
+    triton_omni_expert_func,
+)
+
+__all__ = [
+    "triton_omni_mlp_func",
+    "triton_omni_router_func",
+    "triton_omni_expert_func",
+]
